fe08fcd28666524e3336fc9c45ed5bb501ab45b7
Jarrett Er <happymanjarrett@outlook.com>
2026-01-02 09:30:13 -0500

    temp

--- mt_training/predict_unified.bat
+++ mt_training/predict_unified.bat
@@ -1,70 +1,42 @@
 @echo off
-REM Activate virtualenv and run unified normal prediction script
+
+@REM python predict_unified.py ^
+@REM   --tokenizer_name Thecoder3281f/ManganumT5v1_1-base-separated-augmented-chanlam-fullft-10000 ^
+@REM   --model_path Thecoder3281f/ManganumT5v1_1-base-separated-augmented-chanlam-fullft-10000 ^
+@REM   --dataset_type chanlam_separated ^
+@REM   --chunks all ^
+@REM   --output_dir preds_chanlam/Thecoder3281f/ManganumT5v1_1-base-separated-augmented-chanlam-fullft-10000
+
+@REM python predict_unified.py ^
+@REM   --tokenizer_name Thecoder3281f/ManganumT5v1_1-base-separated-standard-chanlam-fullft-10000 ^
+@REM   --model_path Thecoder3281f/ManganumT5v1_1-base-separated-standard-chanlam-fullft-10000 ^
+@REM   --dataset_type chanlam_separated ^
+@REM   --chunks all ^
+@REM   --output_dir preds_chanlam/Thecoder3281f/ManganumT5v1_1-base-separated-standard-chanlam-fullft-10000
+@REM python predict_unified.py ^
+@REM   --tokenizer_name Thecoder3281f/ManganumT5v1_1-small-separated-augmented-chanlam-fullft-10000 ^
+@REM   --model_path Thecoder3281f/ManganumT5v1_1-small-separated-augmented-chanlam-fullft-10000 ^
+@REM   --dataset_type chanlam_separated ^
+@REM   --chunks all ^
+@REM   --output_dir preds_chanlam/Thecoder3281f/ManganumT5v1_1-small-separated-augmented-chanlam-fullft-10000
+
+
+@REM python predict_unified.py ^
+@REM   --tokenizer_name Thecoder3281f/ManganumT5v1_1-base-separated-augmented-chanlam-fullft-10000 ^
+@REM   --model_path Thecoder3281f/ManganumT5v1_1-base-separated-augmented-chanlam-fullft-10000 ^
+@REM   --dataset_type mit_separated_normal ^
+@REM   --chunks all ^
+@REM   --output_dir preds/Thecoder3281f/ManganumT5v1_1-base-separated-augmented-chanlam-fullft-10000
+
 python predict_unified.py ^
-  --tokenizer_name Thecoder3281f/ManganumT5v1_1-small-separated-standard-chanlam-fullft-2500 ^
-  --model_path Thecoder3281f/ManganumT5v1_1-small-separated-standard-chanlam-fullft-2500 ^
+  --tokenizer_name Thecoder3281f/ManganumT5v1_1-base-separated-standard-chanlam-fullft-10000 ^
+  --model_path Thecoder3281f/ManganumT5v1_1-base-separated-standard-chanlam-fullft-10000 ^
   --dataset_type mit_separated_normal ^
   --chunks all ^
-  --output_dir preds/Thecoder3281f/ManganumT5v1_1-small-separated-standard-chanlam-fullft-2500
-
-@REM REM reevaluate with proper format of data on MIT
-
-@REM python predict_unified.py ^
-@REM   --tokenizer_name GT4SD/multitask-text-and-chemistry-t5-small-standard ^
-@REM   --model_path GT4SD/multitask-text-and-chemistry-t5-small-standard ^
-@REM   --dataset_type mit_separated_normal ^
-@REM   --chunks all ^
-@REM   --output_dir preds/GT4SD/multitask-text-and-chemistry-t5-small-standard
-
-@REM python predict_unified.py ^
-@REM   --tokenizer_name GT4SD/multitask-text-and-chemistry-t5-small-augm ^
-@REM   --model_path GT4SD/multitask-text-and-chemistry-t5-small-augm ^
-@REM   --dataset_type mit_separated_normal ^
-@REM   --chunks all ^
-@REM   --output_dir preds/GT4SD/multitask-text-and-chemistry-t5-small-augm
-
-@REM python predict_unified.py ^
-@REM   --tokenizer_name GT4SD/multitask-text-and-chemistry-t5-base-standard ^
-@REM   --model_path GT4SD/multitask-text-and-chemistry-t5-base-standard ^
-@REM   --dataset_type mit_separated_normal ^
-@REM   --chunks all ^
-@REM   --output_dir preds/GT4SD/multitask-text-and-chemistry-t5-base-standard
-
-@REM python predict_unified.py ^
-@REM   --tokenizer_name GT4SD/multitask-text-and-chemistry-t5-base-augm ^
-@REM   --model_path GT4SD/multitask-text-and-chemistry-t5-base-augm ^
-@REM   --dataset_type mit_separated_normal ^
-@REM   --chunks all ^
-@REM   --output_dir preds/GT4SD/multitask-text-and-chemistry-t5-base-augm
-
-
-
-@REM REM reevaluate with proper format of data on chan lam
-
-@REM python predict_unified.py ^
-@REM   --tokenizer_name GT4SD/multitask-text-and-chemistry-t5-small-standard ^
-@REM   --model_path GT4SD/multitask-text-and-chemistry-t5-small-standard ^
-@REM   --dataset_type chanlam_separated ^
-@REM   --chunks all ^
-@REM   --output_dir preds_chanlam/GT4SD/multitask-text-and-chemistry-t5-small-standard
-
-@REM python predict_unified.py ^
-@REM   --tokenizer_name GT4SD/multitask-text-and-chemistry-t5-small-augm ^
-@REM   --model_path GT4SD/multitask-text-and-chemistry-t5-small-augm ^
-@REM   --dataset_type chanlam_separated ^
-@REM   --chunks all ^
-@REM   --output_dir preds_chanlam/GT4SD/multitask-text-and-chemistry-t5-small-augm
-
-@REM python predict_unified.py ^
-@REM   --tokenizer_name GT4SD/multitask-text-and-chemistry-t5-base-standard ^
-@REM   --model_path GT4SD/multitask-text-and-chemistry-t5-base-standard ^
-@REM   --dataset_type chanlam_separated ^
-@REM   --chunks all ^
-@REM   --output_dir preds_chanlam/GT4SD/multitask-text-and-chemistry-t5-base-standard
-
-@REM python predict_unified.py ^
-@REM   --tokenizer_name GT4SD/multitask-text-and-chemistry-t5-base-augm ^
-@REM   --model_path GT4SD/multitask-text-and-chemistry-t5-base-augm ^
-@REM   --dataset_type chanlam_separated ^
-@REM   --chunks all ^
-@REM   --output_dir preds_chanlam/GT4SD/multitask-text-and-chemistry-t5-base-augm
+  --output_dir preds/Thecoder3281f/ManganumT5v1_1-base-separated-standard-chanlam-fullft-10000
+python predict_unified.py ^
+  --tokenizer_name Thecoder3281f/ManganumT5v1_1-small-separated-augmented-chanlam-fullft-10000 ^
+  --model_path Thecoder3281f/ManganumT5v1_1-small-separated-augmented-chanlam-fullft-10000 ^
+  --dataset_type mit_separated_normal ^
+  --chunks all ^
+  --output_dir preds/Thecoder3281f/ManganumT5v1_1-small-separated-augmented-chanlam-fullft-10000
