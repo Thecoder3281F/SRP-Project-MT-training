9039bef3e54fb1d41f7a9f00378ee8c6427b4a4d
Jarrett <nineplustenequalstwentyone021@gmail.com>
2026-01-05 20:21:33 +0800

    modify batch files a lil

--- mt_training/joincsv.bat
+++ mt_training/joincsv.bat
@@ -1,4 +1,8 @@
 @echo off
 python join_csvs.py ^
---folder "..\_datasets_unprocessed\chanlam_dataset_unprocessed" ^
---output "..\_datasets_unprocessed\chanlam_dataset_unprocessed\mergeddata.csv"
+--folder "model_predictions_42\my_models\afterft\full\mit\Thecoder3281f\ManganumT5v1_1-base-separated-augmented-chanlam-fullft-10000" ^
+--output "model_predictions_42\my_models\afterft\full\mit\Thecoder3281f\ManganumT5v1_1-base-separated-augmented-chanlam-fullft-10000\finalpreds.csv"
+
+python join_csvs.py ^
+--folder "model_predictions_42\my_models\afterft\full\mit\Thecoder3281f\ManganumT5v1_1-small-separated-augmented-chanlam-fullft-10000" ^
+--output "model_predictions_42\my_models\afterft\full\mit\Thecoder3281f\ManganumT5v1_1-small-separated-augmented-chanlam-fullft-10000\finalpreds.csv"
