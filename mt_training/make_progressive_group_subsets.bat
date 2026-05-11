123366b66586d4a93deaa7adefcc4d9c6c547112
Jarrett <nineplustenequalstwentyone021@gmail.com>
2026-05-12 00:20:37 +0800

    add subsets

--- mt_training/make_progressive_group_subsets.bat
+++ mt_training/make_progressive_group_subsets.bat
@@ -2,7 +2,7 @@ python mt_training/make_progressive_group_subsets.py ^
     --input datasets_final/chanlam_final/train_final.csv ^
     --out-dir datasets_final/chanlam_final/progressive_by_reactants ^
     --group-col input_reactants ^
-    --sizes-rows 10,50,100,250,500,1000 ^
+    --sizes-rows 10,50,100,250,500,1000,2000,4000 ^
     --seed 42 ^
     --sample-mode spread ^
     --auto-per-group ^
