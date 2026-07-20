58e54e9ab0c6fbbdb39dd13bc639e3ec96806b41
Jarrett <happymanjarrett@outlook.com>
2026-07-21 07:43:42 +0800

    temp

--- mt_training/train_lora_reactiont5_chanlam.bat
+++ mt_training/train_lora_reactiont5_chanlam.bat
@@ -1,5 +1,5 @@
 python train_lora_reactiont5_chanlam.py ^
     --base_model_name sagawa/ReactionT5v2-forward-USPTO_MIT ^
-    --output_model_dir reactiont5lora ^
-    --num_train_epochs 30 ^
+    --output_model_dir reactiont5lora50rows50epochs ^
+    --num_train_epochs 50 ^
     --use_early_stopping
