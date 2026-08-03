aee828cf97b8b4cf429b7f14a9896cd809962c4c
Jarrett <happymanjarrett@outlook.com>
2026-08-04 07:38:00 +0800

    test

--- train_lora_reactiont5_chanlam.bat
+++ train_lora_reactiont5_chanlam.bat
@@ -1,7 +1,16 @@
 python train_lora_reactiont5_chanlam.py ^
     --base_model_name sagawa/ReactionT5v2-forward-USPTO_MIT ^
-    --output_model_dir reactiont5lora10rows2000steps ^
+    --output_model_dir reactiont5lorachanlamfull2000steps ^
     --max_steps 2000 ^
     --use_early_stopping ^
     --seed 42 ^
-    --train_rows 10
+    --use_full_train_split
+
+python train_lora_reactiont5_chanlam.py ^
+    --base_model_name sagawa/ReactionT5v2-forward-USPTO_MIT ^
+    --output_model_dir reactiont5lora4000rows2000steps ^
+    --max_steps 2000 ^
+    --use_early_stopping ^
+    --seed 42 ^
+    --train_rows 4000 ^
+    --resume_checkpoint
--- utils/joincsv.bat
+++ utils/joincsv.bat
@@ -1,4 +1,16 @@
 @echo off
 python utils/join_csvs.py ^
---folder "preds\Thecoder3281f\ReactionT5-lora-50rows-2k" ^
---output "preds\Thecoder3281f\ReactionT5-lora-50rows-2k\finalpreds.csv"
+--folder "preds\Thecoder3281f\ReactionT5-lora-100rows-2k" ^
+--output "preds\Thecoder3281f\ReactionT5-lora-100rows-2k\finalpreds.csv"
+
+python utils/join_csvs.py ^
+--folder "preds\Thecoder3281f\ReactionT5-lora-500rows-2k" ^
+--output "preds\Thecoder3281f\ReactionT5-lora-500rows-2k\finalpreds.csv"
+
+python utils/join_csvs.py ^
+--folder "preds\Thecoder3281f\ReactionT5-lora-1000rows-2k" ^
+--output "preds\Thecoder3281f\ReactionT5-lora-1000rows-2k\finalpreds.csv"
+
+python utils/join_csvs.py ^
+--folder "preds\Thecoder3281f\ReactionT5-lora-2000rows-2k" ^
+--output "preds\Thecoder3281f\ReactionT5-lora-2000rows-2k\finalpreds.csv"
