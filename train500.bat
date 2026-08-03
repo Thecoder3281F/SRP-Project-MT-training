5b89a37b9d31b513a8228f49fe52da2e81ef82f5
Jarrett <nineplustenequalstwentyone021@gmail.com>
2026-08-03 22:06:12 +0800

    test

--- train500.bat
+++ train500.bat
@@ -1,7 +1,23 @@
 python train_lora_reactiont5_chanlam.py ^
     --base_model_name sagawa/ReactionT5v2-forward-USPTO_MIT ^
+    --output_model_dir reactiont5lora500rows5epochs ^
+    --num_train_epochs 5 ^
+    --use_early_stopping ^
+    --seed 42 ^
+    --train_rows 500
+
+python train_lora_reactiont5_chanlam.py ^
+    --base_model_name sagawa/ReactionT5v2-forward-USPTO_MIT ^
     --output_model_dir reactiont5lora500rows10epochs ^
     --num_train_epochs 10 ^
+    --use_early_stopping ^
+    --seed 42 ^
+    --train_rows 500
+
+python train_lora_reactiont5_chanlam.py ^
+    --base_model_name sagawa/ReactionT5v2-forward-USPTO_MIT ^
+    --output_model_dir reactiont5lora500rows15epochs ^
+    --num_train_epochs 15 ^
     --use_early_stopping ^
     --seed 42 ^
     --train_rows 500
@@ -24,56 +40,8 @@ python train_lora_reactiont5_chanlam.py ^
 
 python train_lora_reactiont5_chanlam.py ^
     --base_model_name sagawa/ReactionT5v2-forward-USPTO_MIT ^
-    --output_model_dir reactiont5lora500rows40epochs ^
-    --num_train_epochs 40 ^
-    --use_early_stopping ^
-    --seed 42 ^
-    --train_rows 500
-
-python train_lora_reactiont5_chanlam.py ^
-    --base_model_name sagawa/ReactionT5v2-forward-USPTO_MIT ^
     --output_model_dir reactiont5lora500rows50epochs ^
     --num_train_epochs 50 ^
-    --use_early_stopping ^
-    --seed 42 ^
-    --train_rows 500
-
-python train_lora_reactiont5_chanlam.py ^
-    --base_model_name sagawa/ReactionT5v2-forward-USPTO_MIT ^
-    --output_model_dir reactiont5lora500rows60epochs ^
-    --num_train_epochs 60 ^
-    --use_early_stopping ^
-    --seed 42 ^
-    --train_rows 500
-
-python train_lora_reactiont5_chanlam.py ^
-    --base_model_name sagawa/ReactionT5v2-forward-USPTO_MIT ^
-    --output_model_dir reactiont5lora500rows70epochs ^
-    --num_train_epochs 70 ^
-    --use_early_stopping ^
-    --seed 42 ^
-    --train_rows 500
-
-python train_lora_reactiont5_chanlam.py ^
-    --base_model_name sagawa/ReactionT5v2-forward-USPTO_MIT ^
-    --output_model_dir reactiont5lora500rows80epochs ^
-    --num_train_epochs 80 ^
-    --use_early_stopping ^
-    --seed 42 ^
-    --train_rows 500
-
-python train_lora_reactiont5_chanlam.py ^
-    --base_model_name sagawa/ReactionT5v2-forward-USPTO_MIT ^
-    --output_model_dir reactiont5lora500rows90epochs ^
-    --num_train_epochs 90 ^
-    --use_early_stopping ^
-    --seed 42 ^
-    --train_rows 500
-
-python train_lora_reactiont5_chanlam.py ^
-    --base_model_name sagawa/ReactionT5v2-forward-USPTO_MIT ^
-    --output_model_dir reactiont5lora500rows100epochs ^
-    --num_train_epochs 100 ^
     --use_early_stopping ^
     --seed 42 ^
     --train_rows 500
--- trainfull.bat
+++ trainfull.bat
@@ -1,79 +1,31 @@
 python train_lora_reactiont5_chanlam.py ^
     --base_model_name sagawa/ReactionT5v2-forward-USPTO_MIT ^
+    --output_model_dir reactiont5lorafull2epochs ^
+    --num_train_epochs 2 ^
+    --use_early_stopping ^
+    --seed 42 ^
+    --use_full_train_split
+
+python train_lora_reactiont5_chanlam.py ^
+    --base_model_name sagawa/ReactionT5v2-forward-USPTO_MIT ^
+    --output_model_dir reactiont5lorafull3epochs ^
+    --num_train_epochs 3 ^
+    --use_early_stopping ^
+    --seed 42 ^
+    --use_full_train_split
+
+python train_lora_reactiont5_chanlam.py ^
+    --base_model_name sagawa/ReactionT5v2-forward-USPTO_MIT ^
+    --output_model_dir reactiont5lorafull5epochs ^
+    --num_train_epochs 5 ^
+    --use_early_stopping ^
+    --seed 42 ^
+    --use_full_train_split
+
+python train_lora_reactiont5_chanlam.py ^
+    --base_model_name sagawa/ReactionT5v2-forward-USPTO_MIT ^
     --output_model_dir reactiont5lorafull10epochs ^
     --num_train_epochs 10 ^
-    --use_early_stopping ^
-    --seed 42 ^
-    --use_full_train_split
-
-python train_lora_reactiont5_chanlam.py ^
-    --base_model_name sagawa/ReactionT5v2-forward-USPTO_MIT ^
-    --output_model_dir reactiont5lorafull20epochs ^
-    --num_train_epochs 20 ^
-    --use_early_stopping ^
-    --seed 42 ^
-    --use_full_train_split
-
-python train_lora_reactiont5_chanlam.py ^
-    --base_model_name sagawa/ReactionT5v2-forward-USPTO_MIT ^
-    --output_model_dir reactiont5lorafull30epochs ^
-    --num_train_epochs 30 ^
-    --use_early_stopping ^
-    --seed 42 ^
-    --use_full_train_split
-
-python train_lora_reactiont5_chanlam.py ^
-    --base_model_name sagawa/ReactionT5v2-forward-USPTO_MIT ^
-    --output_model_dir reactiont5lorafull40epochs ^
-    --num_train_epochs 40 ^
-    --use_early_stopping ^
-    --seed 42 ^
-    --use_full_train_split
-
-python train_lora_reactiont5_chanlam.py ^
-    --base_model_name sagawa/ReactionT5v2-forward-USPTO_MIT ^
-    --output_model_dir reactiont5lorafull50epochs ^
-    --num_train_epochs 50 ^
-    --use_early_stopping ^
-    --seed 42 ^
-    --use_full_train_split
-
-python train_lora_reactiont5_chanlam.py ^
-    --base_model_name sagawa/ReactionT5v2-forward-USPTO_MIT ^
-    --output_model_dir reactiont5lorafull60epochs ^
-    --num_train_epochs 60 ^
-    --use_early_stopping ^
-    --seed 42 ^
-    --use_full_train_split
-
-python train_lora_reactiont5_chanlam.py ^
-    --base_model_name sagawa/ReactionT5v2-forward-USPTO_MIT ^
-    --output_model_dir reactiont5lorafull70epochs ^
-    --num_train_epochs 70 ^
-    --use_early_stopping ^
-    --seed 42 ^
-    --use_full_train_split
-
-python train_lora_reactiont5_chanlam.py ^
-    --base_model_name sagawa/ReactionT5v2-forward-USPTO_MIT ^
-    --output_model_dir reactiont5lorafull80epochs ^
-    --num_train_epochs 80 ^
-    --use_early_stopping ^
-    --seed 42 ^
-    --use_full_train_split
-
-python train_lora_reactiont5_chanlam.py ^
-    --base_model_name sagawa/ReactionT5v2-forward-USPTO_MIT ^
-    --output_model_dir reactiont5lorafull90epochs ^
-    --num_train_epochs 90 ^
-    --use_early_stopping ^
-    --seed 42 ^
-    --use_full_train_split
-
-python train_lora_reactiont5_chanlam.py ^
-    --base_model_name sagawa/ReactionT5v2-forward-USPTO_MIT ^
-    --output_model_dir reactiont5lorafull100epochs ^
-    --num_train_epochs 100 ^
     --use_early_stopping ^
     --seed 42 ^
     --use_full_train_split
