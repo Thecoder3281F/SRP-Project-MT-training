cab9a21be72c1992708274c9b961b5ec16237f82
Jarrett <happymanjarrett@outlook.com>
2026-08-04 07:38:00 +0800

    test

--- predict_unified_lora.bat
+++ predict_unified_lora.bat
@@ -3,14 +3,14 @@ REM Activate virtualenv and run unified LoRA prediction script
 @REM call "C:\Users\user\Desktop\srp\Scripts\Activate.ps1"
 
 
-@REM python predict_unified_lora.py ^
-@REM   --tokenizer_name sagawa/ReactionT5v2-forward-USPTO_MIT ^
-@REM   --base_model_name sagawa/ReactionT5v2-forward-USPTO_MIT ^
-@REM   --lora_adapter_path Thecoder3281f/ReactionT5-lora-10rows-2k ^
-@REM   --dataset_type chanlam_separated ^
-@REM   --chunks all ^
-@REM   --output_dir preds/Thecoder3281f/ReactionT5-lora-10rows-2k ^
-@REM   --prompt_mode "reactiont5"
+python predict_unified_lora.py ^
+  --tokenizer_name sagawa/ReactionT5v2-forward-USPTO_MIT ^
+  --base_model_name sagawa/ReactionT5v2-forward-USPTO_MIT ^
+  --lora_adapter_path Thecoder3281f/ReactionT5-lora-chanlamfull-2k ^
+  --dataset_type chanlam_separated ^
+  --chunks all ^
+  --output_dir preds/Thecoder3281f/ReactionT5-lora-chanlamfull-2k ^
+  --prompt_mode "reactiont5"
 
 python predict_unified_lora.py ^
   --tokenizer_name sagawa/ReactionT5v2-forward-USPTO_MIT ^
@@ -19,5 +19,4 @@ python predict_unified_lora.py ^
   --dataset_type chanlam_separated ^
   --chunks all ^
   --output_dir preds/Thecoder3281f/ReactionT5-lora-10rows-2k ^
-  --prompt_mode "reactiont5" ^
-  --no-bf16
+  --prompt_mode "reactiont5"
