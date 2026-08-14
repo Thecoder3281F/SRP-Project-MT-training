f718b2643c304f13cc1a8f5a392af1c7b701e4a3
Jarrett <nineplustenequalstwentyone021@gmail.com>
2026-08-14 23:42:39 +0800

    ig bro

--- make_progressive_group_subsets.bat
+++ make_progressive_group_subsets.bat
@@ -1,9 +1,29 @@
 python make_progressive_group_subsets.py ^
     --input _datasets_final/chanlam_final/train_final.csv ^
-    --out-dir _datasets_final/chanlam_final/progressive_splits/seed123 ^
+    --out-dir _datasets_final/chanlam_final/progressive_splits/seed1111 ^
     --group-col input_reactants ^
     --sizes-rows 10,50,100,250,500,1000,2000,4000 ^
-    --seed 123 ^
+    --seed 1111 ^
+    --sample-mode spread ^
+    --auto-per-group ^
+    --exclude-full
+
+python make_progressive_group_subsets.py ^
+    --input _datasets_final/chanlam_final/train_final.csv ^
+    --out-dir _datasets_final/chanlam_final/progressive_splits/seed67 ^
+    --group-col input_reactants ^
+    --sizes-rows 10,50,100,250,500,1000,2000,4000 ^
+    --seed 67 ^
+    --sample-mode spread ^
+    --auto-per-group ^
+    --exclude-full
+
+python make_progressive_group_subsets.py ^
+    --input _datasets_final/chanlam_final/train_final.csv ^
+    --out-dir _datasets_final/chanlam_final/progressive_splits/seed420 ^
+    --group-col input_reactants ^
+    --sizes-rows 10,50,100,250,500,1000,2000,4000 ^
+    --seed 420 ^
     --sample-mode spread ^
     --auto-per-group ^
     --exclude-full
