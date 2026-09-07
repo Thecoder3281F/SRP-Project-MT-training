677df0ea67bdfeaf08912a19ca1dac0b3fa58ccd
Jarrett <nineplustenequalstwentyone021@gmail.com>
2026-09-07 11:34:54 +0800

    dd

--- make_progressive_group_subsets.bat
+++ make_progressive_group_subsets.bat
@@ -1,29 +1,9 @@
 python make_progressive_group_subsets.py ^
-    --input _datasets_final/chanlam_final/train_final.csv ^
-    --out-dir _datasets_final/chanlam_final/progressive_splits/seed1111 ^
-    --group-col input_reactants ^
-    --sizes-rows 10,50,100,250,500,1000,2000,4000 ^
+    --input _datasets_final/buchwald_hartwig_final/train.csv ^
+    --out-dir _datasets_final/buchwald_hartwig_final/progressive_splits/seed1111 ^
+    --group-col reactants ^
+    --sizes-rows 10,50,100,250,500,1000,2000,3000 ^
     --seed 1111 ^
-    --sample-mode spread ^
-    --auto-per-group ^
-    --exclude-full
-
-python make_progressive_group_subsets.py ^
-    --input _datasets_final/chanlam_final/train_final.csv ^
-    --out-dir _datasets_final/chanlam_final/progressive_splits/seed67 ^
-    --group-col input_reactants ^
-    --sizes-rows 10,50,100,250,500,1000,2000,4000 ^
-    --seed 67 ^
-    --sample-mode spread ^
-    --auto-per-group ^
-    --exclude-full
-
-python make_progressive_group_subsets.py ^
-    --input _datasets_final/chanlam_final/train_final.csv ^
-    --out-dir _datasets_final/chanlam_final/progressive_splits/seed420 ^
-    --group-col input_reactants ^
-    --sizes-rows 10,50,100,250,500,1000,2000,4000 ^
-    --seed 420 ^
     --sample-mode spread ^
     --auto-per-group ^
     --exclude-full
